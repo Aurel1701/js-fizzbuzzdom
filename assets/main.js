c3169c5c16991d71c213b3cf51cad239e7982924
Aurel1701 <gantzfan96@gmail.com>
2022-03-28 14:32:32 +0200

    edit

--- assets/main.js
+++ assets/main.js
@@ -1,12 +1,20 @@
 const numbers = document.querySelector('.numbers')
 
 for (let i = 1; i <= 100; i++){
-    
+    if(i % 3 == 0 && i % 5 !== 0){
+        numbers.insertAdjacentHTML("beforeend", `<div class="number fizz">Fizz</div>`)
 
-    numbers.insertAdjacentHTML("beforeend", `<div class="number">${i}</div>`)
+    }else if(i % 5 == 0 && i % 3 !== 0){
+        numbers.insertAdjacentHTML("beforeend", `<div class="number buzz">Buzz</div>`)
 
-  
+    }else if(i % 3 == 0 && i % 5 == 0){
+        numbers.insertAdjacentHTML("beforeend", `<div class="number fizzbuzz">FizzBuzz</div>`)
 
-   
+    }else{
+        numbers.insertAdjacentHTML("beforeend", `<div class="number normal">${i}</div>`)
+
+    }
 
 }
+
+
